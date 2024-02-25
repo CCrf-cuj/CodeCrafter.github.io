d671a60ce55dea43c17d32b8d2aa007e8e12aa7e
Krish Kumar <122594525+Imkkrish@users.noreply.github.com>
2024-02-25 16:41:40 +0530

    Update script.js
    update member page functions

--- script.js
+++ script.js
@@ -57,20 +57,36 @@ const sentences = [
 
 printSentenceWithBlinking(sentences); // Call the function with the array of sentences
 
-
+//Funtions for Members page Start from here-------->>>>
 // Function to switch to Members content
 document.getElementById('membersButton').addEventListener('click', function() {
   document.getElementById('alumniContent').classList.remove('active');
   document.getElementById('membersContent').classList.add('active');
+
+  // Change text properties of buttons
+  document.getElementById('alumniButton').style.textShadow = ''; // Reset text-shadow
+  document.getElementById('alumniButton').style.color = ''; // Reset color
+  document.getElementById('membersButton').style.textShadow = '0 0 20px rgba(0, 0, 0, 1)'; // Change text-shadow
+  document.getElementById('membersButton').style.color = 'rgb(255, 253, 253)'; // Change color
 });
 
 // Function to switch to Alumni content
 document.getElementById('alumniButton').addEventListener('click', function() {
   document.getElementById('membersContent').classList.remove('active');
   document.getElementById('alumniContent').classList.add('active');
+
+  // Change text properties of buttons
+  document.getElementById('membersButton').style.textShadow = ''; // Reset text-shadow
+  document.getElementById('membersButton').style.color = ''; // Reset color
+  document.getElementById('alumniButton').style.textShadow = '0 0 20px rgba(0, 0, 0, 1)'; // Change text-shadow
+  document.getElementById('alumniButton').style.color = 'rgb(255, 253, 253)'; // Change color
 });
 
 // Set the Members content to be visible by default
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('membersContent').classList.add('active');
+
+  // Apply text properties to the Members button by default
+  document.getElementById('membersButton').style.textShadow = '0 0 20px rgba(0, 0, 0, 1)';
+  document.getElementById('membersButton').style.color = 'rgb(255, 253, 253)';
 });
